e271e5428e99ebf7117e72573ff8c69cff1d5d14
LouiseMustafaeva <112573505+LouiseMustafaeva@users.noreply.github.com>
2024-01-16 18:43:19 +0500

    Creating of Location files

---
+++ ToDoApp/Model/Location.swift
@@ -0,0 +1,12 @@
+//
+//  Location.swift
+//  ToDoApp
+//
+//  Created by Louise on 15/01/24.
+//
+
+import Foundation
+
+struct Location: Equatable {
+    let name: String
+}
---
+++ ToDoAppTests/Model/LocationTests.swift
@@ -0,0 +1,21 @@
+//
+//  LocationTests.swift
+//  ToDoAppTests
+//
+//  Created by Louise on 15/01/24.
+//
+
+import XCTest
+@testable import ToDoApp
+
+final class LocationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
